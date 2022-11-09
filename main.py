292cbc6f4dfb7edca5ca5010d6525fff2d80ca5b
Felipe Santos <72233926+Felipesco@users.noreply.github.com>
2022-11-09 09:31:10 -0300

    Update main.py

--- main.py
+++ main.py
@@ -11,6 +11,8 @@ def index():
     geral = "<h1>Consultas</h1>"
     return (geral)
 
+#=============== GET ==================
+
 @app.route('/consulta', methods=['GET'])
 def consulta_geral():
     mycursor.execute("SELECT * FROM tbpedido")
@@ -18,10 +20,33 @@ def consulta_geral():
     return jsonify(pedidos)
 
 @app.route('/consulta/<int:id>', methods=['GET'])
-def consulta_id():
-    mycursor.execute("SELECT ")
+def consulta_id(id):
+    id = str(id)
+    mycursor.execute("SELECT idPedido, periodo, curso, lab, computador, titulo, descPedido, nomeProf, nomeAluno	FROM tbPedido WHERE idPedido = "+id)
+    pedidos = mycursor.fetchall()
+    return jsonify(pedidos)
 
+#============== POST ================
 
+@app.route('/cadastro', methods=['POST'])
+def cadastro_pedido():
+
+    request_data = request.get_json()
+
+    periodo = request_data["periodo"]
+    curso = request_data["curso"]
+    lab = request_data["lab"]
+    computador = request_data["computador"]
+    titulo = request_data["titulo"]
+    descPedido = request_data["descPedido"]
+    nomeProf = request_data["nomeProf"]
+    nomeAluno = request_data["nomeAluno"]
+    turma =  request_data["turma"]
+
+    mycursor.execute("INSERT INTO tbpedido (periodo, curso, lab, computador, titulo, descPedido, nomeProf, nomeAluno, turma) VALUES('"+periodo+"', '"+curso+"', '"+lab+"', '"+computador+"', '"+titulo+"', '"+descPedido+"','"+nomeProf+"','"+nomeAluno+"', '"+turma+"' );")
+    cadastro = mycursor.fetchall()
+    ok = "ok"
+    return jsonify(ok, cadastro)
 
 
 
