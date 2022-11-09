811089d9fac2658718f809623945ac1c50622d4d
Felipe Santos <72233926+Felipesco@users.noreply.github.com>
2022-11-09 08:06:15 -0300

    Update main.py

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import mysql.connector
 
 app = Flask(__name__)
 
-mydb = mysql.connector.connect(host="localhost", user="root", password="", database="bdetecapi")
+mydb = mysql.connector.connect(host="localhost", user="root", password="", database="bdetecapi2")
 mycursor = mydb.cursor()
 
 @app.route('/', methods=['GET'])
@@ -11,4 +11,6 @@ def index():
     geral = "<h1>Consultas</h1>"
     return (geral)
 
-@app
+@app.route('/consulta', methods=['GET'])
+def consulta_geral():
+    mycursor.execute("SELECT * FROM tbpedido")
